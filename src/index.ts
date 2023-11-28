2a429e0eb3f255c075ce9a5109d6fa0a7c58df9f
Julian Totzek-Hallhuber <j.totzek@gmail.com>
2023-11-28 11:40:03 +0100

    inital commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import * as core from '@actions/core'
+import { request } from '@octokit/request'
+import * as github from '@actions/github'
+import { env } from "process";
+
+// get input params
+let parameters:any = {}
+
+const token = core.getInput('github_token', {required: true} );
+parameters['token'] = token
+
+const environment = process.env
+
+core.info(`environment: ${JSON.stringify(environment)}`)
+
