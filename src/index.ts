4a0cbb716eb54aa8057b06503de9443ccd8a1bc6
Julian Totzek-Hallhuber <j.totzek@gmail.com>
2023-11-28 12:40:36 +0100

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
-import { request } from '@octokit/request'
+import * as octokit from '@octokit/request'
+import { Octokit } from '@octokit/rest'
 import * as github from '@actions/github'
 import { env } from "process";
 
@@ -10,6 +11,21 @@ const token = core.getInput('token', {required: true} );
 parameters['token'] = token
 
 const environment = process.env
-
+const gitHubOrgName = environment['GITHUB_REPOSITORY_OWNER']
+core.info(`gitHubOrgName: ${gitHubOrgName}`)
 core.info(`environment: ${JSON.stringify(environment)}`)
 
+
+
+const auth = new Octokit({
+    auth: parameters.token
+})
+  
+const secrets = octokit.request('GET /orgs/gitHubOrgName/actions/secrets', {
+    org: gitHubOrgName,
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+})
+
+core.info(`secrets: ${JSON.stringify(secrets)}`)
